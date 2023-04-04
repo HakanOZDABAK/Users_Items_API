54bf09acc4b7e6fa1c365c54bc42065c8f4c7594
Hakan ÖZDABAK <127786293+HakanOZDABAK@users.noreply.github.com>
2023-04-04 14:40:00 +0300

    Update database_setup.py

--- app/databases/database_setup.py
+++ app/databases/database_setup.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("postgresql://zachrqkq:Dg7jthN8WEiXs8WwHm61Z0hJzUju_fhW@rogue.db.elephantsql.com/zachrqkq",echo=True)
+engine = create_engine("YOUR DATABASE ENGINE URL",echo=True)
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
